a33ee3c8bcfb0bca02f4d8194ea6ba55f39eb719
fr0ste <73338az@gmail.com>
2024-05-30 19:35:34 -0600

    se agrega un ejemplo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { ChatComponent } from './components/chat/chat.component';
+import { TableComponent } from './components/table/table.component';
 
 export const routes: Routes = [
   {
     path: 'chat/:userId',
     component: ChatComponent,
   },
+  {
+    path: 'tablero',
+    component: TableComponent
+  }
 ];
--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,25 +1,18 @@
-import { ChatService } from './../../services/chat.service';
-import { ChatMessage } from './../../models/chat-message';
-import { Component, OnInit, inject } from '@angular/core';
-import { CompatClient, Stomp } from '@stomp/stompjs';
-import SockJS from 'sockjs-client';
-import { ActivatedRoute } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { NgFor, NgIf } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ChatMessage } from './../../models/chat-message';
+import { ChatService } from './../../services/chat.service';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [
-  FormsModule,
-  NgFor,
-  NgIf
-  ],
+  imports: [FormsModule, NgFor, NgIf],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.css',
 })
-export class ChatComponent implements OnInit{
-
+export class ChatComponent implements OnInit {
   /**
    * variables
    */
@@ -34,37 +27,33 @@ export class ChatComponent implements OnInit{
   private route = inject(ActivatedRoute);
 
   ngOnInit(): void {
-      this.chatService.joinRoom("ABC");
+    this.chatService.joinRoom('ABC');
 
-      this.userId = this.route.snapshot.params["userId"];
+    this.userId = this.route.snapshot.params['userId'];
 
-      this.listenerMessage();
+    this.listenerMessage();
 
-      console.log("message list: ", this.messageList);
+    console.log('message list: ', this.messageList);
   }
 
   public sendMessage() {
     const chatMessage = {
-      message : this.messageInput,
-      user: this.userId
+      message: this.messageInput,
+      user: this.userId,
     } as ChatMessage;
 
+    this.chatService.sendMessage('ABC', chatMessage);
 
-    this.chatService.sendMessage("ABC" , chatMessage);
-
-    this.messageInput = "";
-
+    this.messageInput = '';
   }
 
-  public listenerMessage(){
-    this.chatService.getMessageSubject().subscribe((messages: any ) => {
+  public listenerMessage() {
+    this.chatService.getMessageSubject().subscribe((messages: any) => {
       this.messageList = messages.map((item: any) => ({
         ...item,
-        message_side: item.user === this.userId ? 'sender' : 'reciver'
-      }))
+        message_side: item.user === this.userId ? 'sender' : 'reciver',
+      }));
     });
     console.log(this.messageList);
   }
-
-
 }
