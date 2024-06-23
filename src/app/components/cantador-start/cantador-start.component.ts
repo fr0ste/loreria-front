08ba8dd0566f62677d508857f22b2c54ce0ffffc
Sainos <qa.kadasoftware@gmail.com>
2024-06-23 02:38:22 -0600

    Validación en "cantador-start"

--- src/app/components/cantador-start/cantador-start.component.ts
+++ src/app/components/cantador-start/cantador-start.component.ts
@@ -2,13 +2,16 @@ import { WebSocketService } from './../../services/web-socket.service';
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Game } from '../../models/games';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
+
 
 @Component({
   selector: 'app-cantador-start',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   templateUrl: './cantador-start.component.html',
   styleUrl: './cantador-start.component.css'
@@ -17,11 +20,20 @@ export class CantadorStartComponent {
   public maxPlayers: number = 4; // Puedes ajustar este valor o hacerlo configurable en la vista
 
   constructor(private webSocketService: WebSocketService, private router: Router) {}
-
+/*
   createGame() {
     this.webSocketService.startGame(this.maxPlayers).subscribe((game: Game) => {
       console.log('Game created:', game);
       this.router.navigate(['cards', game.gameId]); // Redirigir a la vista del deck del juego
     });
+  }
+}*/
+  createGame(form: NgForm) {
+    if (form.valid) {
+      this.webSocketService.startGame(this.maxPlayers).subscribe((game: Game) => {
+        console.log('Game created:', game);
+        this.router.navigate(['cards', game.gameId]);
+      });
+    }
   }
 }
