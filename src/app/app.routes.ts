a0de3f5ffe260920f71cec82540df993392af5f1
fr0ste <73338az@gmail.com>
2024-06-23 22:40:09 -0600

    se eliminan componentes no utilizados

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,13 @@
 import { Routes } from '@angular/router';
-import { ChatComponent } from './components/chat/chat.component';
-import { TableComponent } from './components/table/table.component';
-import { CardsComponent } from './components/cards/cards.component';
-import { LoteriaComponent } from './components/loteria/loteria.component';
-import { WaitingComponent } from './components/waiting/waiting.component';
-import { LobbyPlayerComponent } from './components/lobby-player/lobby-player.component';
 import { CantadorStartComponent } from './components/cantador-start/cantador-start.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { LobbyPlayerComponent } from './components/lobby-player/lobby-player.component';
 import { LoginComponent } from './components/login/login.component';
+import { TableComponent } from './components/table/table.component';
+import { WaitingComponent } from './components/waiting/waiting.component';
 
 
 export const routes: Routes = [
-  {
-    path: 'chat/:userId',
-    component: ChatComponent,
-  },
   {
     path: 'table/:game/:player',
     component: TableComponent
